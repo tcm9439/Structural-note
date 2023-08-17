b85a942a5fefbb2228cbac885ebbbdce0274fcdb
tcm9439 <tsechingmaisy@gmail.com>
2023-08-17 21:09:11 +0800

    Add vue basic example.

---
+++ Structural-app/components/vue/note.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Note } from "structural-core"
+
+const props = defineProps<{
+    note: Note,
+}>()
+
+</script>
+
+<template>
+    <h1>{{ props.note.title }}</h1>
+    <template  v-for="(value, index) of props.note.sections.components" :key="value[0]">
+        <vue-section-text-section :text_section="value[1]" />
+    </template>
+</template>
---
+++ Structural-app/components/vue/section/text-section.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { TextAreaSection } from "structural-core"
+
+const props = defineProps<{
+    text_section: TextAreaSection,
+}>()
+
+</script>
+
+<template>
+    <h2>{{ props.text_section.title }}</h2>
+    <div>
+        <p>{{ props.text_section.content }}</p>
+    </div>
+</template>
