7987700f7fdc8a30353baeb88c7328f544b6dece
tcm9439 <tsechingmaisy@gmail.com>
2023-09-10 22:43:55 +0800

    Update computed to watch => less reload

--- Structural-app/components/mt/element/structural.vue
+++ Structural-app/components/mt/element/structural.vue
@@ -8,8 +8,13 @@ const props = defineProps<{
 const editing_note: Ref<Note> | undefined = ref(inject("editing-note"))
 const struct_element = editing_note === undefined? null : ref(props.edit_path.getNodeByPath(editing_note.value) as StructuralElement)
 
-const getValues = computed(() => {
-    console.log("struct element edit_path", props.edit_path)
+type ElementValue = {
+    id: string,
+    type: string,
+    path: EditPath,
+}
+
+function getElementsValues(): ElementValue[]{
     return struct_element?.value.stepInEachChildren(props.edit_path).map((child_path) => {
         const child_id = child_path.getLastStep()
         let child = null
@@ -25,13 +30,25 @@ const getValues = computed(() => {
             type: child?.definition_type_str,
             path: child_path,
         }
-    })
-})
+    }) ?? []
+}
+
+// const getValues = computed(() => {
+//     console.log("struct element edit_path", props.edit_path)
+//     return getElementsValues()
+// })
+
+const elements_values: Ref<null | ElementValue[]> = ref(null)
+// watch the length of the struct_element.value.values => update elements_values
+watch(() => struct_element?.value?.values.size, () => {
+    elements_values.value = getElementsValues()
+}, { immediate: true })
 
 </script>
 
 <template>
-    <template v-for="value in getValues">
+    <template v-for="value in elements_values">
+    <!-- <template v-for="value in getValues"> -->
         <template v-if="value.type === 'STRING'">
             <mt-attribute-value-string :edit_path="value.path"></mt-attribute-value-string>
         </template> 
