9aef2ab25f83d6ce89571f81fe353fb0bac58426
tcm9439 <tsechingmaisy@gmail.com>
2023-10-21 16:55:08 +0800

    Update header

--- Structural-app/components/mt/layout/header.vue
+++ Structural-app/components/mt/layout/header.vue
@@ -6,21 +6,23 @@ const { $viewState, $emitter } = useNuxtApp();
 
 const editing_note_name = ref<string>($viewState.editing_note_name)
 
-function menuSelectHandler(menu_item: string){
+// # menu
+const refresh_menu = ref(0)
+
+async function menuSelectHandler(menu_item: string){
     console.log("menu select", menu_item)
     switch (menu_item){
         case "open-file":
-            console.log("open file")
             NoteFileHandler.openNote()
             break
         case "save-file":
-            console.log("save file")
             NoteFileHandler.saveNote()
             break
         case "save-as-file":
             NoteFileHandler.saveNote(true)
             break
     }
+    refresh_menu.value++
 }
 
 // # listen to open note event (from here or the menu)
@@ -34,37 +36,60 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-    <Menu mode="horizontal" theme="dark" @on-select="menuSelectHandler">
-        <MenuItem name="title">
-            {{ editing_note_name }}
-        </MenuItem>
-        <Submenu name="file-operation">
-            <template #title>
-                <Icon type="ios-stats" />
-                File
-            </template>
-            <MenuItem name="open-file">Open</MenuItem>
-            <MenuItem name="save-file">Save</MenuItem>
-            <MenuItem name="save-as-file">Save As</MenuItem>
-        </Submenu>
-        <MenuItem name="setting">
-            <Icon type="md-settings" />
-            Setting
-        </MenuItem>
-    </Menu>
+    <Header>
+        <Row>
+            <Col>
+                <span class="note-name">
+                    {{editing_note_name}}
+                </span>
+            </Col>
+            <Col>
+                <Menu mode="horizontal" theme="dark" :key="refresh_menu"
+                    @on-select="menuSelectHandler"
+                >
+                    <Submenu name="file-operation">
+                        <template #title>
+                            <Icon type="ios-stats" />
+                            File
+                        </template>
+                        <MenuItem name="open-file">Open</MenuItem>
+                        <MenuItem name="save-file">Save</MenuItem>
+                        <MenuItem name="save-as-file">Save As</MenuItem>
+                    </Submenu>
+                    <MenuItem name="setting">
+                        <Icon type="md-settings" />
+                        Setting
+                    </MenuItem>
+                </Menu>
+            </Col>
+        </Row>
+    </Header>
 </template>
 
 <style scoped>
-    .ivu-menu-horizontal {
-        position: fixed;
-        width: 100%;
-        z-index: 20;
-        max-height: max(8vh, 40px);
-        line-height: max(8vh, 40px);
-    }
-    
-    .white-text {
-        padding-right: 10px;
-        color: #fff;
-    }
+.ivu-layout-header {
+    position: fixed;
+    width: 100%;
+    z-index: 20;
+    max-height: max(8vh, 40px);
+    line-height: max(8vh, 40px);
+}
+
+.ivu-menu-horizontal {
+    max-height: max(8vh, 40px);
+    line-height: max(8vh, 40px);
+}
+
+.ivu-menu-item {
+    padding: 0 10px;
+}
+
+.ivu-menu-submenu {
+    padding: 0 10px;
+}
+
+.note-name {
+    color: #fff;
+    padding-right: 10px;
+}
 </style>
